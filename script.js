c597e436e7739b267496a72b6a97bc97e56cf925
Vaibhav Sharma <bhardwajvaibhav9760@gmail.com>
2026-03-03 12:00:00 +0530

    Add navigation logic from landing page to registration page

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 let incomeTransactions = [];
 let expenseTransactions = [];
 let allTransactions = [];
+// Registration 
+document.getElementById("goRegister").addEventListener("click", () => {
+  document.getElementById("landingPage").style.display = "none";
+  document.getElementById("registerPage").style.display = "block";
+});
